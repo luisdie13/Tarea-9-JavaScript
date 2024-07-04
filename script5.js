181fa6b854afa41186060dd2a95e03f196d517a8
Luis Diego <ldluisdie@gmail.com>
2024-07-03 19:47:14 -0600

    cambios en ejercicio

--- script5.js
+++ script5.js
@@ -36,25 +36,25 @@ return highestPriorityEnemy ? highestPriorityEnemy.name : null;
 
 while (true) {
 
-const enemy1Name = prompt("Ingrese nombre del enemigo 1:");
-const enemy1DistanceStr = prompt("Ingrese distancia del enemigo 1:");
+const enemy1Name = prompt("Ingrese nombre del enemigo 1: ");
+const enemy1DistanceStr = prompt("Ingrese distancia del enemigo 1: ");
 const enemy1Distance = parseFloat(enemy1DistanceStr);
-const enemy1PriorityStr = prompt("Ingrese prioridad del enemigo 1:");
+const enemy1PriorityStr = prompt("Ingrese prioridad del enemigo 1: ");
 const enemy1Priority = parseInt(enemy1PriorityStr);
-const enemy1SpeedStr = prompt("Ingrese velocidad del enemigo 1:");
+const enemy1SpeedStr = prompt("Ingrese velocidad del enemigo 1: ");
 const enemy1Speed = parseInt(enemy1SpeedStr);
 
-const enemy2Name = prompt("Ingrese nombre del enemigo 2:");
-const enemy2DistanceStr = prompt("Ingrese distancia del enemigo 2:");
+const enemy2Name = prompt("Ingrese nombre del enemigo 2: ");
+const enemy2DistanceStr = prompt("Ingrese distancia del enemigo 2: ");
 const enemy2Distance = parseFloat(enemy2DistanceStr);
-const enemy2PriorityStr = prompt("Ingrese prioridad del enemigo 2:");
+const enemy2PriorityStr = prompt("Ingrese prioridad del enemigo 2: ");
 const enemy2Priority = parseInt(enemy2PriorityStr);
-const enemy2SpeedStr = prompt("Ingrese velocidad del enemigo 2:");
+const enemy2SpeedStr = prompt("Ingrese velocidad del enemigo 2: ");
 const enemy2Speed = parseInt(enemy2SpeedStr);
 
 updateEnemy(enemy1Name, enemy1Distance, enemy1Priority, enemy1Speed);
 updateEnemy(enemy2Name, enemy2Distance, enemy2Priority, enemy2Speed);
 
 const highestPriorityEnemyName = findHighestPriorityClosestFastestEnemy();
-console.log("El enemigo con mayor prioridad, más cercano y más veloz para atacar es:", highestPriorityEnemyName);
+console.log("El enemigo con mayor prioridad, más cercano y más veloz para atacar es: ", highestPriorityEnemyName);
 }
